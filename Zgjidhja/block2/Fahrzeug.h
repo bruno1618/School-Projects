3b42a956d54ee990eafdf6eff901eaa1e75a8d89
bruno1618 <35975116+bruno1618@users.noreply.github.com>
2018-01-31 10:38:33 +0100

    Update Fahrzeug.h
    kot

--- Zgjidhja/block2/Fahrzeug.h
+++ Zgjidhja/block2/Fahrzeug.h
@@ -2,7 +2,7 @@
 
 #include "AktivesVO.h"
 #include "FzgVerhalten.h"
-#include "SimuClient.h"
+
 
 class Weg;//Weg wird bekannt gemacht
 
@@ -11,33 +11,33 @@ class Fahrzeug :
 {
 	public:
 		Fahrzeug(); //Standartkonstruktor
-		Fahrzeug(const string sName);//Konstruktor mit Namensübergabe
-		Fahrzeug(const string sName, const double dMaxgeschwindigkeit);//Konstruktor mit Namensübergabe und MaxGeschwindigkeit
+		Fahrzeug(const string sName);//Konstruktor mit NamensÃ¼bergabe
+		Fahrzeug(const string sName, const double dMaxgeschwindigkeit);//Konstruktor mit NamensÃ¼bergabe und MaxGeschwindigkeit
 		virtual ~Fahrzeug();//Destruktor
-		virtual void vAusgabe() const; //gibt Fahrzeugspezifische Informationen über die Textausgabe aus
-		virtual void vAbfertigung();//Abfertigungsschritt, aktualisiert Fahrzeugspezifische Informationen gemäß der Globalen Zeit
+		virtual void vAusgabe() const; //gibt Fahrzeugspezifische Informationen Ã¼ber die Textausgabe aus
+		virtual void vAbfertigung();//Abfertigungsschritt, aktualisiert Fahrzeugspezifische Informationen gemÃ¤ÃŸ der Globalen Zeit
 		virtual double dTanken(double dMenge = -1);//Tankt das Fahrzeug
-		virtual double dGeschwindigkeit() const;// gibt die aktuelle Geschwindigkeit des Fahrzeugs zurück
-		virtual ostream& ostreamAusgabe(ostream& out) const;//Ausgabe über die Überladung des << Operators
+		virtual double dGeschwindigkeit() const;// gibt die aktuelle Geschwindigkeit des Fahrzeugs zurÃ¼ck
+		virtual ostream& ostreamAusgabe(ostream& out) const;//Ausgabe Ã¼ber die Ãœberladung des << Operators
 		virtual istream& istreamEingabe(istream& in);
-		bool operator<(const Fahrzeug& Fzg) const;//Überladung des < Operators, vergleicht die gefahrene Strecke von zwei Fahrzeugen
-		bool operator>(const Fahrzeug& Fzg) const;//Überladung des > Operators, vergleicht die gefahrene Strecke von zwei Fahrzeugen
-		Fahrzeug& operator=(const Fahrzeug& Fzg);//Überladung des Zuweisungsoperators, kopiert Objekte und weißt eine neue ID zu
-												 //bool operator==(const Fahrzeug& Fzg) const;//Überladung des == Operators, vergleicht die gefahrene Strecke von zwei Fahrzeugen
+		bool operator<(const Fahrzeug& Fzg) const;//Ãœberladung des < Operators, vergleicht die gefahrene Strecke von zwei Fahrzeugen
+		bool operator>(const Fahrzeug& Fzg) const;//Ãœberladung des > Operators, vergleicht die gefahrene Strecke von zwei Fahrzeugen
+		Fahrzeug& operator=(const Fahrzeug& Fzg);//Ãœberladung des Zuweisungsoperators, kopiert Objekte und weiÃŸt eine neue ID zu
+												 //bool operator==(const Fahrzeug& Fzg) const;//Ãœberladung des == Operators, vergleicht die gefahrene Strecke von zwei Fahrzeugen
 		void vNeueStrecke(Weg* pWeg);//Setzt das _Fahrzeug auf einen neuen Weg
-		void vNeueStrecke(Weg * pWeg, double dStartzeitpunkt);//Überladung der Funktion, gibt zusätzlich einen Startzeitpunkt an zu dem das Fahrzeug los fahren soll
-		double getAbschnittsStrecke() const;//Get-Funktion für p_dAbschnittsStrecke
-		virtual void vZeichnen(const Weg* pWeg) const;//Macht in Fahrzeug nichts, wird in beiden Unterklassen überschrieben
+		void vNeueStrecke(Weg * pWeg, double dStartzeitpunkt);//Ãœberladung der Funktion, gibt zusÃ¤tzlich einen Startzeitpunkt an zu dem das Fahrzeug los fahren soll
+		double getAbschnittsStrecke() const;//Get-Funktion fÃ¼r p_dAbschnittsStrecke
+		virtual void vZeichnen(const Weg* pWeg) const;//Macht in Fahrzeug nichts, wird in beiden Unterklassen Ã¼berschrieben
 
 	private:
-		Fahrzeug(const Fahrzeug& Fzg);//Copykonstruktor des Fahrzeugs, auf private damit er nicht zufällig benutzt wird
+		Fahrzeug(const Fahrzeug& Fzg);//Copykonstruktor des Fahrzeugs, auf private damit er nicht zufÃ¤llig benutzt wird
 		void vInitialisierung(); // Initialisiert Membervariablen auf 0 oder "" und setzt die ID
 
 	protected:
 		double p_dMaxGeschwindigkeit; //Maximale GGeschwindigkeit des Fahrzeugs
 		double p_dGesamtstrecke; //Gesamt gefahrene Strecke des Fahrzeugs
 		double p_dGesamtzeit; //Gesamtfahrzeit des Fahrzeugs
-		double p_dAbschnittStrecke; //zurückgelegte Strecke auf dem aktuellen Abschnitt
+		double p_dAbschnittStrecke; //zurÃ¼ckgelegte Strecke auf dem aktuellen Abschnitt
 		FzgVerhalten* p_pVerhalten;//Legt das Verhalten auf einem Weg fest, 
 };
 
